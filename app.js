d2911e01a4a14c23fea24028b336530b555ac94b
unknown <pemeraldy@gmail.com>
2020-05-04 07:32:18 +0100

    little changes to setup

--- app.js
+++ app.js
@@ -135,38 +135,38 @@ const deleteUser = (req, res) => {
   })
 }
 
-/*
-app.get('/api/v1/tours', getAllTours);
-app.get('/api/v1/tours/:id', getTour);
-app.post('/api/v1/tours', createTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
-*/
+const tourRouter = express.Router()
+const userRouter = express.Router()
 
-app
-  .route('/api/v1/tours')
+
+tourRouter
+  .route('/')
   .get(getAllTours)
   .post(createTour)
 
 
-app
-  .route('/api/v1/tours/:id')
+tourRouter
+  .route('/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour)
 
 /**Users middleware**/
-app
-  .route('/api/v1/users')
+
+
+userRouter
+  .route('/')
   .get(getAllUsers)
   .post(createUser)
 
-app
-  .route('/api/v1/user/:id')
+userRouter
+  .route('/:id')
   .get(getUser)
   .patch(updateUser)
   .delete(deleteUser)
 
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
 
 app.listen(port, () => console.log('App running on port ' + port));
